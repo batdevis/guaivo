1e71b4c188b81db091a4faeb7662e4c8c249a6ed
batdevis <batdevis@gmail.com>
2016-05-31 23:52:40 +0200

    project notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531213952) do
+ActiveRecord::Schema.define(version: 20160531214843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160531213952) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "notes"
   end
 
   create_table "swagger_definitions", force: :cascade do |t|
